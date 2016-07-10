bacf40d47a86938d17bc4faf5440a71ee29df89c
Hugh Dickins <hughd@google.com>
2016-07-10 16:46:32 -0700

    tmpfs: fix regression hang in fallocate undo
    commit 7f556567036cb7f89aabe2f0954b08566b4efb53 upstream.
    
    The well-spotted fallocate undo fix is good in most cases, but not when
    fallocate failed on the very first page.  index 0 then passes lend -1
    to shmem_undo_range(), and that has two bad effects: (a) that it will
    undo every fallocation throughout the file, unrestricted by the current
    range; but more importantly (b) it can cause the undo to hang, because
    lend -1 is treated as truncation, which makes it keep on retrying until
    every page has gone, but those already fully instantiated will never go
    away.  Big thank you to xfstests generic/269 which demonstrates this.
    
    Fixes: b9b4bb26af01 ("tmpfs: don't undo fallocate past its last page")
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    
    Conflicts:
    	mm/shmem.c

--- mm/shmem.c
+++ mm/shmem.c
@@ -1959,6 +1959,9 @@ static long shmem_fallocate(struct file *file, int mode, loff_t offset,
 			/* Remove the !PageUptodate pages we added */
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d3f0dd0... tmpfs: fix regression hang in fallocate undo
 =======
 >>>>>>> d3f0dd0... tmpfs: fix regression hang in fallocate undo
 			if (index > start) {
@@ -1967,11 +1970,14 @@ static long shmem_fallocate(struct file *file, int mode, loff_t offset,
 				 ((loff_t)index << PAGE_CACHE_SHIFT) - 1, true);
 			}
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 			shmem_undo_range(inode,
 				(loff_t)start << PAGE_CACHE_SHIFT,
 				((loff_t)index << PAGE_CACHE_SHIFT) - 1, true);
 >>>>>>> 80c759d... tmpfs: don't undo fallocate past its last page
+=======
+>>>>>>> d3f0dd0... tmpfs: fix regression hang in fallocate undo
 =======
 >>>>>>> d3f0dd0... tmpfs: fix regression hang in fallocate undo
 			goto undone;
